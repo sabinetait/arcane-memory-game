cdfa5d44eaf1e99529cfbca1c6f241ac3864c76d
Sabine Tait <sabine.tait@gmail.com>
2022-04-14 14:56:28 -0400

    added logic to check matching cards

--- scrips.js
+++ scrips.js
@@ -39,27 +39,48 @@ const generateCards = () => {
             front.src = character.src;
             front.alt = character.alt;
             back.src = './assets/card-back-face.webp';
-            back.alt = 'back-face'
+            back.alt = 'back-face';
+            card.setAttribute('id', character.id);
             // Append to game container
             gameContainer.appendChild(card);
             card.appendChild(front);
             card.appendChild(back);
+            // Card Flip
+            card.addEventListener('click', (event) => {
+                card.classList.toggle('flipCard');
+                checkCards(event);
+            })
         })
     }
     randomizer();
 }
 generateCards();
 
-// Card Flip
-const cards = document.querySelectorAll('.card');
-function flipCard() {
-    this.classList.toggle('flip');
-}
-cards.forEach((card) => {
-    card.addEventListener('click', flipCard);
-}) 
-
 // Logic for card matching
-// Keep a counter
+// user has to pick two cards
+// if both cards match they remain front facing
+// else if both cards do not match they turn back over and user has to select again
+const checkCards = (event) => {
+    const selectedCard = event.target;
+    selectedCard.classList.toggle('isFlipped');
+    const flippedCards = document.querySelectorAll('.isFlipped');
+    // two cards have been selected
+    if(flippedCards.length === 2) {
+        if(flippedCards[0].id === flippedCards[1].id) { 
+            flippedCards.forEach(card => {
+                card.classList.remove('isFlipped');
+                card.style.pointerEvents = 'none';
+            })
+        } else {
+            flippedCards.forEach(card => {
+                card.classList.remove('isFlipped');
+                setTimeout(() => card.classList.remove('flipCard'), 1200);
+            })
+        }
+    }
+}
+checkCards();
 // Reset at the end of the game
+// Keep a counter?
+
 
